5ceacb34f23a2b861e891322fd8dc277fb840996
Shamilkhan <shamilkhan1999@gmail.com>
2020-11-09 04:35:22 +0300

    hide sqlite

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,9 +1,9 @@
 module.exports = {
- // type: 'postgres',
- // url: process.env.DATABASE_URI,
+ type: 'postgres',
+ url: process.env.DATABASE_URI,
  // use sqlite for local dev
- type: "sqlite",
- database: "database.sqlite",
+ // type: "sqlite",
+ // database: "database.sqlite",
  synchronize: true,
  logging: true,
  entities: ["src/entity/**/*.ts"],
